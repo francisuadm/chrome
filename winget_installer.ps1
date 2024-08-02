9b41d3ecdbfb5fe038c47a90479e3e8527149c94
francisuadm <francisuadm@gmail.com>
2024-08-02 10:19:22 -0700

    Update winget_installer.ps1

--- winget_installer.ps1
+++ winget_installer.ps1
@@ -1,9 +1,10 @@
-powershell -Command "
+# PowerShell script to install winget on Windows 10
+
 # Set the URL for the App Installer package
 $appInstallerUrl = 'https://aka.ms/Microsoft.Store.AppInstaller'
 
 # Define the path to save the downloaded package
-$downloadPath = '$env:TEMP\AppInstaller.appxbundle'
+$downloadPath = "$env:TEMP\AppInstaller.appxbundle"
 
 # Download the App Installer package
 Invoke-WebRequest -Uri $appInstallerUrl -OutFile $downloadPath
@@ -17,4 +18,3 @@ if (Get-Command winget -ErrorAction SilentlyContinue) {
 } else {
     Write-Output 'winget installation failed.'
 }
-"
