157f16c7a17a9f75a83c46c22608ae30a19caabb
francisuadm <francisuadm@gmail.com>
2024-08-14 14:21:46 -0700

    Delete add_to_localadmin.ps1

--- add_to_localadmin.ps1
+++
@@ -1,8 +0,0 @@
-# Define the username to be added to the Administrators group
-$domain = Read-Host "Enter domain"
-$username = Read-Host "Enter username"
-$fullUsername = "$domain\$username"
-
-
-# Add the user to the Administrators group
-Add-LocalGroupMember -Group "Administrators" -Member $fullUsername
