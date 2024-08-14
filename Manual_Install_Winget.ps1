0634cd4559b346cff6dbb70783c48eac71b5b8dd
francisuadm <francisuadm@gmail.com>
2024-08-14 08:24:19 -0700

    Update Manual_Install_Winget.ps1

--- Manual_Install_Winget.ps1
+++ Manual_Install_Winget.ps1
@@ -7,3 +7,24 @@ Expand-Archive -Path .\UI.Xaml.2.8.6.nupkg.zip -DestinationPath C:\IT_Folder\UI.
 Add-AppxPackage -Path C:\IT_Folder\UI.Xaml.2.8.6\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx
 Add-AppxPackage -Path C:\IT_Folder\DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 winget
+
+
+
+
+
+“No applicable app licenses found” error when trying to run winget. This issue can occur if the necessary app licenses are not properly installed or recognized by the system.
+
+This this to resolve this issue:
+
+Download the license file from this link.
+https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/08d8788d59cf47ed9bf42c31e31f8efa_License1.xml
+
+Install the License File:
+Use the Add-AppxProvisionedPackage command to add the license file to your system. Run the following command in PowerShell:
+Add-AppxProvisionedPackage -Online -PackagePath "C:\IT_Folder\08d8788d59cf47ed9bf42c31e31f8efa_License1.xml" -LicensePath "C:\IT_Folder\08d8788d59cf47ed9bf42c31e31f8efa_License1.xml"
+
+Reinstall the Packages:
+Ensure that all the necessary packages are installed correctly. You can use the following commands:
+Add-AppxPackage -Path C:\IT_Folder\Desktop.appx
+Add-AppxPackage -Path C:\IT_Folder\UI.Xaml.2.8.6\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx
+Add-AppxPackage -Path C:\IT_Folder\DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
