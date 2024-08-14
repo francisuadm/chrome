9928488ceb6908f9d8f6c81307d668a650443839
francisuadm <francisuadm@gmail.com>
2024-08-14 14:20:25 -0700

    Delete AD_as_A_Account.ps1

--- AD_as_A_Account.ps1
+++
@@ -1,7 +0,0 @@
-# Prompt for the domain and username
-$domain = Read-Host "Enter domain"
-$username = Read-Host "Enter username"
-$fullUsername = "$domain\$username"
-
-# Run the command with the specified user
-Start-Process -FilePath "powershell.exe" -ArgumentList "-Command `"Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command `"dsa.msc`"' -Verb RunAs`"" -Credential $fullUsername
