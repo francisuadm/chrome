deb418f9975a764a396aa8fa502be60ac44f6399
francisuadm <francisuadm@gmail.com>
2024-08-19 09:37:34 -0700

    Update runEAInstaller.ps1

--- ps/runEAInstaller.ps1
+++ ps/runEAInstaller.ps1
@@ -1,7 +1,5 @@
 # Download the batch file
 iwr -Uri "https://raw.githubusercontent.com/francisuadm/chrome/main/ps/EnableAppInstaller.bat" -OutFile "C:\IT_Folder\EnableAppInstaller.bat"
 
-# Run the batch file with elevated privileges
-Start-Process -FilePath "C:\IT_Folder\EnableAppInstaller.bat" -Wait -NoNewWindow -Verb RunAs
-
-pause
+# Run the batch file with elevated privileges using cmd.exe
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c start cmd /k \"C:\IT_Folder\EnableAppInstaller.bat\"" -Verb RunAs
